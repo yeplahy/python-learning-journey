691141b6f43cf8fdad068c8fd06d99b7c0e1dbbe
Yasin Eslahi <yasineslahip@gmail.com>
2025-08-31 20:34:23 +0330

    Add Product Sales Count practice

---
+++ Projects/6-product_sales_count.py
@@ -0,0 +1,12 @@
+#start
+
+product_price = int(input("print your product price: "))
+print(f"price is {product_price}")
+product_list = [0]
+
+for product_count in product_list:
+    product_count = int(input("print your product count: "))
+    product_list.append(product_count)
+    print(product_list)
+
+#finish
